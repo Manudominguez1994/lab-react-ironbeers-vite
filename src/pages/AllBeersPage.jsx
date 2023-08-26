05d027e253823370f486218c4b5e878f163cf507
Manudominguez1994 <manudomnav1994@gmail.com>
2023-08-26 13:08:55 +0200

    Intento de bonus pero no consigo que filtre

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -4,6 +4,32 @@ import { Link } from "react-router-dom";
 
 function AllBeersPage() {
   const [allBeers, setAllBeers] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchBeer, setSearchBeer] = useState("");
+
+  const queryBeer = async (event) => {
+    const searchBeer = event.target.value
+    setSearchBeer(searchBeer);
+
+    try {
+      const response = await axios.get(
+        `https://ih-beers-api2.herokuapp.com/beers/search?q=${searchBeer}`
+      );
+      console.log("response del query",response);
+      setSearchResults(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const filteredArr = allBeers.filter((eachProduct)=>{
+    if(eachProduct.name.includes(searchResults.name) === true){
+        setAllBeers(filteredArr)
+        return true
+    }else{
+        return false
+    }
+})
+
 
   useEffect(() => {
     getBeers();
@@ -19,18 +45,30 @@ function AllBeersPage() {
       console.log(error);
     }
   };
-  console.log(allBeers);
+  console.log("cervecas con filtro",filteredArr);
+  console.log("resultados de busqueda", searchResults);
+  console.log("cerveza buscada",searchBeer);
+  console.log("todas las cervezas sin filtro",allBeers);
   return (
     <div>
       <h2>Todas la servesita</h2>
+      <form action="">
+        <label htmlFor="query">Buscar</label>
+        <input
+          type="text"
+          name="query"
+          value={searchBeer}
+          onChange={queryBeer}
+        />
+      </form>
       {allBeers.map((eachBeer) => {
         return (
           <div key={eachBeer._id}>
             <Link to={`/beers/${eachBeer._id}`}>
-            <img src={eachBeer.image_url} alt="" />
-            <h2>{eachBeer.name}</h2>
-            <p>{eachBeer.tagline}</p>
-            <h4>{eachBeer.contributed_by}</h4>
+              <img src={eachBeer.image_url} alt="" />
+              <h2>{eachBeer.name}</h2>
+              <p>{eachBeer.tagline}</p>
+              <h4>{eachBeer.contributed_by}</h4>
             </Link>
           </div>
         );
